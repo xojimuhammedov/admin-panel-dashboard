7bcf3eb3dccbb386e08d101e780c46cbbdee3eb4
MacBook Air <macbookair@MacBook-Air-MacBook.local>
2025-02-16 22:50:13 +0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [data, setData] = useState([])
+  return (
+    <div className='app'>
+      Hello world
+    </div>
+  )
+}
+
+export default App
---
+++ src/services/request.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import storage from "./storage";
+import config from "../config";
+
+const request = axios.create({
+    headers: {
+        "Content-Type": 'application/json'
+    },
+    baseURL: config.API_URL,
+    params: {}
+})
+
+request.interceptors.request.use(
+    (config) => {
+        const token = storage.get("token")
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => {
+        console.log(error)
+        return Promise.reject(error)
+    }
+)
+
+request.interceptors.response.use(
+    (response) => {
+        return response
+    },
+    (error) => {
+        const statusCode = error.response
+        if (statusCode === 401) {
+            window.localStorage.clear()
+        }
+        return Promise.reject(error)
+    }
+)
+
+export { request }
